7568bacfc5ffbd1844defacc8a00b0d55c8bef8e
antoniszczepanik <antoni.szczepanik@greensteam.com>
2019-10-02 14:14:11 +0200

    add xclip

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,6 +5,8 @@ apt install -y git tmux vim-gtk3
 
 setxkbmap -option caps:swapescape
 
+apt-get install xclip
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
