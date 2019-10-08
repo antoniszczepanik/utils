646a1929761cbcb8460b8c6050b4f3119b590b95
antoniszczepanik <antoni.szczepanik@greensteam.com>
2019-10-08 10:20:36 +0200

    add new aliases and gitdiff as a tool

--- setup/setup.sh
+++ setup/setup.sh
@@ -10,6 +10,11 @@ apt-get install xclip
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+git config --global diff.tool vimdiff
+git config --global difftool.prompt 0
+git config --global merge.tool vimdiff
+git config --global alias.s status
+
 echo "Remember to run 'CTRL+B I' to install tmux plugins and to run :PluginInstall in vim"
 
 
