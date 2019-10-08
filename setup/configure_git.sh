ff738bd56e7c392393f1efeaf2d6d79c149cb83f
antoniszczepanik <antoni.szczepanik@greensteam.com>
2019-10-08 10:43:17 +0200

    move git config to seperate script

---
+++ setup/configure_git.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# dotfile contains secrets 
+git config --global diff.tool vimdiff
+git config --global difftool.prompt 0
+git config --global merge.tool vimdiff
+git config --global alias.s status
+
