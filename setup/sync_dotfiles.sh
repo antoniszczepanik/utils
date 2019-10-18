e8096583d435ca1d57590dfe2f3fb26c4c7e09fe
antoniszczepanik <antoni.szczepanik@greensteam.com>
2019-10-18 14:04:00 +0200

    add gnome conf

--- setup/sync_dotfiles.sh
+++ setup/sync_dotfiles.sh
@@ -5,3 +5,4 @@ cp ~/.bashrc dotfiles/
 cp ~/.tmux.conf dotfiles/
 cp -rL ~/.tmux/resurrect/last dotfiles/resurrect/
 cp ~/.config/htop/htoprc dotfiles/
+dconf dump /org/gnome/terminal/ > dotfiles/gnome_terminal_settings.txt
