41a963cb023c4634f8c52956168fb109adf16555
kabuboy <szczepanik.antoni@gmail.com>
2019-10-18 21:33:16 +0200

    make sure bashrc is used inside tmux

--- setup/apply_dotfiles.sh
+++ setup/apply_dotfiles.sh
@@ -7,3 +7,5 @@ sudo cp -r dotfiles/resurrect ~/.tmux/resurrect
 mkdir -p ~/.config/htop/htoprc
 sudo cp dotfiles/htoprc ~/.config/htop/htoprc
 dconf load /org/gnome/terminal/ < dotfiles/gnome_terminal_settings.txt
+# make sure bashrc is sourced in tmux env
+echo ". ~/.bashrc" >> ~/.bash_profile
