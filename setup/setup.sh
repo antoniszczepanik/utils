91825b811cef1bd35b421575fdefba0c86b638e6
antoniszczepanik <antoni.szczepanik@greensteam.com>
2019-10-18 12:15:41 +0200

    automate vims plugininstall

--- setup/setup.sh
+++ setup/setup.sh
@@ -14,5 +14,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ./configure_git.sh
+./apply_dotfiles.sh
+sudo vim +PluginInstall +qall
 
-echo "Remember to run 'CTRL+B I' to install tmux plugins and to run :PluginInstall in vim"
+echo "Remember to run 'CTRL+B I' to install tmux plugins!"
