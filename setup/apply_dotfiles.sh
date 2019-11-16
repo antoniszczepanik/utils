687c56d25e56c57d6992b2e25eddd3a820005193
kabuboy <szczepanik.antoni@gmail.com>
2019-11-16 19:47:05 +0100

    correct inconsistencies for ubuntu19.10

--- setup/apply_dotfiles.sh
+++ setup/apply_dotfiles.sh
@@ -3,9 +3,13 @@
 cp dotfiles/.vimrc ~/.vimrc
 cp dotfiles/.bashrc ~/.bashrc
 cp dotfiles/.tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.tmux/resurrect
 sudo cp -r dotfiles/resurrect ~/.tmux/resurrect
+
 mkdir -p ~/.config/htop/htoprc
 sudo cp dotfiles/htoprc ~/.config/htop/htoprc
+
 dconf load /org/gnome/terminal/ < dotfiles/gnome_terminal_settings.txt
 # make sure bashrc is sourced in tmux env
 echo ". ~/.bashrc" >> ~/.bash_profile
