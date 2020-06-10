eea7613d18ce2f6d2bc02ed14f2a7802c2460438
antoniszczepanik <antoni.szczepanik@greensteam.com>
2020-06-10 16:33:44 +0200

    Update cp script and bump isort version

--- scripts/gs/cp_files_for_analysis_report_testing.py
+++ scripts/gs/cp_files_for_analysis_report_testing.py
@@ -26,12 +26,12 @@ s3_cmd_4 = f'aws s3 sync s3://greensteam-data-prod/fouling_analysis/ s3://greens
 s3_cmd_5 = f'aws s3 sync s3://greensteam-data-prod/unification/ s3://greensteam-data-dev/{user}/unification/ --exclude="*" --include="*{vessel_slug}*"'
 s3_cmd_6 = f'aws s3 sync s3://greensteam-models-prod/fouling_analysis/{vessel_slug}/ s3://greensteam-models-dev/antoni.szczepanik/fouling_analysis/{vessel_slug}/'
 
-minio_cmd_1 = f'aws s3 sync s3://greensteam-data-prod/customer_data/ ~/data/greensteam-data-dev/{user}/customer_data/ --exclude="*" --include="*{vessel_slug}*"'
-minio_cmd_2 = f'aws s3 sync s3://greensteam-data-prod/final/ ~/data/greensteam-data-dev/{user}/final/ --exclude="*" --include="*{vessel_slug}*"'
-minio_cmd_3 = f'aws s3 sync s3://greensteam-prod/data-import/{customer_slug}/advanced/ ~/data/greensteam-dev/data-import/{customer_slug}/advanced/'
-minio_cmd_4 = f'aws s3 sync s3://greensteam-data-prod/fouling_analysis/ ~/data/greensteam-data-dev/{user}/fouling_analysis/ --exclude="*" --include="*{vessel_slug}*"'
-minio_cmd_5 = f'aws s3 sync s3://greensteam-data-prod/unification/ ~/data/greensteam-data-dev/{user}/unification/ --exclude="*" --include="*{vessel_slug}*"'
-minio_cmd_6 = f'aws s3 sync s3://greensteam-models-prod/fouling_analysis/{vessel_slug}/ ~/data/greensteam-models-dev/antoni.szczepanik/fouling_analysis/{vessel_slug}/'
+minio_cmd_1 = f'aws s3 sync s3://greensteam-data-prod/customer_data/ ~/data/greensteam-data/customer_data/ --exclude="*" --include="*{vessel_slug}*"'
+minio_cmd_2 = f'aws s3 sync s3://greensteam-data-prod/final/ ~/data/greensteam-data/final/ --exclude="*" --include="*{vessel_slug}*"'
+minio_cmd_3 = f'aws s3 sync s3://greensteam-prod/data-import/{customer_slug}/advanced/ ~/data/greensteam/data-import/{customer_slug}/advanced/'
+minio_cmd_4 = f'aws s3 sync s3://greensteam-data-prod/fouling_analysis/ ~/data/greensteam-data/fouling_analysis/ --exclude="*" --include="*{vessel_slug}*"'
+minio_cmd_5 = f'aws s3 sync s3://greensteam-data-prod/unification/ ~/data/greensteam-data/unification/ --exclude="*" --include="*{vessel_slug}*"'
+minio_cmd_6 = f'aws s3 sync s3://greensteam-models-prod/fouling_analysis/{vessel_slug}/ ~/data/greensteam-models/fouling_analysis/{vessel_slug}/'
 
 if minio:
     cmds = [minio_cmd_1, minio_cmd_2, minio_cmd_3, minio_cmd_4, minio_cmd_5, minio_cmd_6]
