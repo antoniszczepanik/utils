c54ed063fec612b7ff07a5774d029e9aafb4029e
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2020-10-03 11:30:48 +0200

    Add notes taking support to todo script and add aliases for it in .zshrc

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
 DATE_TODAY=$(date +'%A, %d %B %Y')
-TODO="$HOME/todo"
-TODO_FILE="$TODO/todo.txt"
+TODO_DIR="$HOME/todo"
+TODO_FILE="$TODO_DIR/todo.txt"
 
 if [ "$1" == "priv" ]; then
-  TODO_FILE="$TODO/todo_priv.txt"
+  TODO_FILE="$TODO_DIR/todo_priv.txt"
+elif [ "$1" == "note" ]; then
+  TODO_FILE="$TODO_DIR/note.txt"
 fi
 
-mkdir -p $TODO
+mkdir -p $TODO_DIR
 
-if ! ls -a $TODO | grep -q .git; then
-	git clone https://github.com/antoniszczepanik/todo.git $TODO
+# if not a git direcotry clone existing
+if ! ls -a $TODO_DIR | grep -q .git; then
+	git clone https://github.com/antoniszczepanik/todo.git $TODO_DIR
 fi
 
-# If there's not todays date in a file try pulling
+# If there's no todays date in a file try pulling
 if ! cat $TODO_FILE | grep -q "$DATE_TODAY"; then
-	cd $TODO && git pull && cd -
+	cd $TODO_DIR && git pull && cd -
 fi
 
 # Try once again and if still not append it
@@ -27,7 +30,7 @@ if ! cat $TODO_FILE | grep -q "$DATE_TODAY"; then
 	echo >> $TODO_FILE
 fi
 
-vim + $TODO_FILE && cd $TODO && git add -A && git commit -m 'update' > /dev/null
+vim + $TODO_FILE && cd $TODO_DIR && git add -A && git commit -m 'update' > /dev/null
 
 # if commit made succesfully push in background
 [ $? -eq 0 ] && git push </dev/null &>/dev/null &
--- setup/apply_dotfiles.sh
+++ setup/apply_dotfiles.sh
@@ -2,4 +2,5 @@
 
 cp dotfiles/.vimrc ~/.vimrc
 cp dotfiles/.tmux.conf ~/.tmux.conf
+cp dotfiles/.zshrc ~/.zshrc
 cp -r dotfiles/regolith/ ~/.config/
