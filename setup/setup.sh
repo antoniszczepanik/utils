bb4a295a1b18e3a9312651dd554eeb8961a6610f
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2020-12-22 17:38:30 +0100

    Add comment about rebinding esc->tab

--- setup/setup.sh
+++ setup/setup.sh
@@ -14,6 +14,9 @@ apt install -y \
 	python3-pip \
   zsh
 
+# Rebind escape with tab with
+# dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
+
 usermod -aG docker $USER
 # install compose
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
