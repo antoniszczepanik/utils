27261b050d717a034aba54c49490b447c5bac973
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2021-03-18 23:37:39 +0100

    Add commit author rewrite script

---
+++ scripts/rewrite_multiple_commits_author.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="Antoni Szczepanik"
+CORRECT_NAME="Antoni Szczepanik"
+CORRECT_EMAIL="szczepanik.antoni@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
