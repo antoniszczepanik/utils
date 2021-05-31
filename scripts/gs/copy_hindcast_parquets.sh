ef2dbc739f629a76a8702494a299ccaa66ccb5aa
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2021-05-31 15:51:57 +0200

    Delete copy_hindcast_parquets.sh

--- scripts/gs/copy_hindcast_parquets.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-aws s3 sync s3://greensteam-data-prod/unification/download_depth_data/ s3://greensteam-data-dev/antoni.szczepanik/unification/download_depth_data/ --exclude "*" --include "*JDNPedro*"
-aws s3 sync s3://greensteam-data-prod/unification/download_hindcast_data/ s3://greensteam-data-dev/antoni.szczepanik/unification/download_hindcast_data/ --exclude "*" --include "*JDNPedro*"
-aws s3 cp s3://greensteam-data-prod/ais/JDNPedroAlvarezCabral.parquet s3://greensteam-data-dev/antoni.szczepanik/ais/JDNPedroAlvarezCabral.parquet
-
